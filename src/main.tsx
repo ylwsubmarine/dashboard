243b2c09a2f1230849260c76967828379435d623
ylwsubmarine <154047361+ylwsubmarine@users.noreply.github.com>
2025-04-09 15:48:31 +0400

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '@styles/GlobalStyles';
+import { theme } from '@styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+); 
